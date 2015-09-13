876f70d7ae59937c95cdb374d0fe238cbcf6709b
Denis P <lsden1995@gmail.com>
2015-09-13 20:00:31 +0300

    Complete first task

---
+++ BSUIRJS/BSUIRJS/Controllers/FirstTaskController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace BSUIRJS.Controllers
+{
+	public class FirstTaskController : Controller
+	{
+		public ActionResult First()
+		{
+			return View();
+		}
+
+		public ActionResult Second()
+		{
+			return View();
+		}
+
+		public ActionResult Third()
+		{
+			return View();
+		}
+	}
+}
---
+++ BSUIRJS/BSUIRJS/Scripts/First/Third.js
@@ -0,0 +1,10 @@
+﻿var resultName = prompt("Введите сюда своё имя", "Введите имя");
+alert("Ваше имя " + resultName);
+
+var isResultToContinue = confirm("Начать заново?");
+
+while (isResultToContinue) {
+	isResultToContinue = confirm("Не надоело?");
+}
+
+alert("Ну и правильно!");
