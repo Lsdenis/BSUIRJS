752178bf17ae73477bf587cdfec5e6a787f789c1
Denis P <lsden1995@gmail.com>
2015-10-02 13:56:47 +0300

    second task

---
+++ BSUIRJS/BSUIRJS/Controllers/SecondTaskController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace BSUIRJS.Controllers
+{
+    public class SecondTaskController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
